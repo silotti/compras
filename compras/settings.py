11e0f02218c07e9d2fdd1ffbfd26986cb366b494
silotti <silotti@gmail.com>
2025-01-14 16:05:25 -0300

    correção do pythonanyhere

--- compras/settings.py
+++ compras/settings.py
@@ -57,6 +57,7 @@ TEMPLATES = [
 ]
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 
 MIDDLEWARE = [
@@ -131,11 +132,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
-
-STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
